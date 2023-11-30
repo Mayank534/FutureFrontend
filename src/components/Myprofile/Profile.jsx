c57bc43c1b753633a39444bfc23c751f91506704
AshNewar <jnvspr12345@gmail.com>
2023-11-30 14:56:50 +0530

    Ge

--- src/components/Myprofile/Profile.jsx
+++ src/components/Myprofile/Profile.jsx
@@ -11,10 +11,15 @@ const Profile = () => {
             </div>
             <div className='order-book-content'>
                 <h2>OrderBook</h2>
+                <div className="func-content">
+                    <div className="buy"><h3>Buy</h3></div>
+                    <div className="sell"><h3>Sell</h3></div>
+
+                </div>
             </div>
 
         </div>
     )
 }
 
-export default Profile
+export default Profile
